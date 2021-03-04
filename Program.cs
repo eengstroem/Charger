52c64f46695f929a4c8189389f3c9ce817091245
Marius Hambro Bovbjerg <marius@hambro.eu>
2021-03-04 13:50:22 +0100

    test fix

--- Program.cs
+++ Program.cs
@@ -1,44 +1,12 @@
-﻿class Program
+﻿using System;
+
+namespace Charger.App.Console
 {
-    static void Main(string[] args)
+    class Program
     {
-        // Assemble your system here from all the classes
-
-        bool finish = false;
-        do
+        static void Main(string[] args)
         {
-            string input;
-            System.Console.WriteLine("Indtast E, O, C, R: ");
-            input = Console.ReadLine();
-            if (string.IsNullOrEmpty(input)) continue;
-
-            switch (input[0])
-            {
-                case 'E':
-                    finish = true;
-                    break;
-
-                case 'O':
-                    door.OnDoorOpen();
-                    break;
-
-                case 'C':
-                    door.OnDoorClose();
-                    break;
-
-                case 'R':
-                    System.Console.WriteLine("Indtast RFID id: ");
-                    string idString = System.Console.ReadLine();
-
-                    int id = Convert.ToInt32(idString);
-                    rfidReader.OnRfidRead(id);
-                    break;
-
-                default:
-                    break;
-            }
-
-        } while (!finish);
+            Console.WriteLine("Hello World!");
+        }
     }
-}
 }
